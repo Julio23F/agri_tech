bc398c696ab683327db4b019e6edc1a85884032a
Julio23F <juliofaralahy23@gmail.com>
2024-03-12 13:30:55 +0300

    Ajout du dossier arduino

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
-import 'dart:async';
-import 'package:bluetooth/BlueController.dart';
-import 'package:bluetooth/page_deux.dart';
-// import 'package:bluetooth/controllers/bluetooth_controller.dart';
-import 'package:bluetooth/views/home_page.dart';
 import 'package:bluetooth/views/start_page.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() {
   runApp(const MyApp());
